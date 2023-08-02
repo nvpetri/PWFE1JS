cb47ade55aa915fddc654a7101f6220d14487aba
NPS <nicolaspetri18@gmail.com>
2023-08-02 16:26:08 -0300

    pega ai fons

--- Aula1/main.js
+++ Aula1/main.js
@@ -1,1 +1,22 @@
 'use strict'
+
+function trocarTitulo(){
+    const tituloAtual = document.getElementById('titulo')
+    const tituloNovo = prompt('Digite um titulo')
+    tituloAtual.textContent = tituloNovo
+}
+
+const botaoTrocarTitulo = document.getElementById('trocar-titulo')
+botaoTrocarTitulo.addEventListener('click', trocarTitulo)
+
+    /* serve apenas para mostrar no console, não aparece na tela
+    console,log(conteudo)
+
+    criando variaveis: (palavras reservadas);
+    var nome = ana
+    let nome = ana
+    const nome = ana
+
+    3 - var -> Mutável -> Global
+    2 - let -> Mutável -> Bloco {}
+    1 - const -> Imutável -> Bloco {} */
