01da791459b096c87f03162302c8a6764fc9387b
NPS <nicolaspetri18@gmail.com>
2023-08-02 16:45:17 -0300

    Nome escola e cadastro aluno

--- Aula1/main.js
+++ Aula1/main.js
@@ -3,11 +3,27 @@
 function trocarTitulo(){
     const tituloAtual = document.getElementById('titulo')
     const tituloNovo = prompt('Digite um titulo')
-    tituloAtual.textContent = tituloNovo
+    tituloAtual.textContent = `Escola ${tituloNovo}`
 }
 
 const botaoTrocarTitulo = document.getElementById('trocar-titulo')
 botaoTrocarTitulo.addEventListener('click', trocarTitulo)
+
+function cadastrar(){
+    const nome1 = document.getElementById('nome')
+    const nome = prompt('Digite seu nome')
+    nome1.textContent = `Nome: ${nome}`
+
+    const idade1 = document.getElementById('idade')
+    const idade = prompt('Digite sua idade')
+    idade1.textContent = `Idade: ${idade}`
+
+    const cidade1 = document.getElementById('cidade')
+    const cidade = prompt('Digite sua cidade')
+    cidade1.textContent = `Cidade: ${cidade}`
+}
+const botaoCadastrar = document.getElementById('cadastrarbutton')
+botaoCadastrar.addEventListener('click', cadastrar)
 
     /* serve apenas para mostrar no console, não aparece na tela
     console,log(conteudo)
