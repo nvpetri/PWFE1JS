61625ef115b47bc989c28e21911c971a78e6e5a1
NPS <nicolaspetri18@gmail.com>
2023-08-09 16:14:35 -0300

    pt1 finish

--- Aula2/app.js
+++ Aula2/app.js
@@ -8,6 +8,7 @@ const forma5 = document.getElementById('forma5')
 const forma6 = document.getElementById('forma6')
 const forma7 = document.getElementById('forma7')
 const forma8 = document.getElementById('forma8')
+const tudo = document.getElementById('container')
 
 function mudarCor(){
     forma1.classList.toggle('vermelho')
@@ -17,4 +18,28 @@ function mudarForma(){
     forma2.classList.toggle('circulo')
 }forma2.addEventListener('click', mudarForma)
 
-function mudarTamanho(){}
+function mudarTamanho(){
+    forma3.classList.toggle('zoom')
+}forma3.addEventListener('click', mudarTamanho)
+
+function girar() {
+    forma4.classList.toggle('giro')
+    forma4.classList.toggle('circulo')
+    forma4.classList.toggle('img2')
+}forma4.addEventListener('click', girar)
+
+function imagem() {
+    forma5.classList.toggle('imagem')
+}forma5.addEventListener('click', imagem)
+
+function windowsUp() {
+    window.location.href = "https://fakeupdate.net/win7/"
+}forma6.addEventListener('click', windowsUp)
+
+function trocar() {
+    forma7.classList.toggle('mover')
+}forma7.addEventListener('click', trocar)
+
+function sumir(){
+    tudo.classList.toggle('sumir')
+}forma8.addEventListener('click', sumir)
