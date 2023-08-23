0519670d4ade5eae48f461360b3ca978b59c5937
NPS <nicolaspetri18@gmail.com>
2023-08-23 15:38:28 -0300

    add na main

--- aula4/app.js
+++ aula4/app.js
@@ -5,7 +5,8 @@ const mostrarTodos = document.getElementById('mostrar-todos')
 function mostrarTodosNumeros(){
     const numeros = document.getElementById('numeros').value.split(',')
     const container = document.getElementById('container-numeros')
- 
+    
+    container.replaceChildren('')
     const ultimoIndice = numeros.length
 
     for(let contador = 0; contador < ultimoIndice ; contador ++){
