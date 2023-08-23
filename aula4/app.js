8dc03a9742f5ae87a69edc78b3ef7740cce14b3d
NPS <nicolaspetri18@gmail.com>
2023-08-23 16:10:26 -0300

    back

--- aula4/app.js
+++ aula4/app.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const mostrarTodos = document.getElementById('mostrar-todos')
+const botaoMostrarPares = document.getElementById('mostrar-pares')
+const botaoMostrarImpares = document.getElementById('mostrar-impares')
 
 function mostrarTodosNumeros(){
     const numeros = document.getElementById('numeros').value.split(',')
@@ -16,5 +18,41 @@ function mostrarTodosNumeros(){
     }
 
 }
+
+function mostrarPares(){
+    const numeros = document.getElementById('numeros').value.split(',')
+    const container = document.getElementById('cont-mostrar-pares')
+
+    container.replaceChildren('')
+    const ultimoIndice = numeros.length
+
+    for(let contador = 0; contador < ultimoIndice ; contador ++){
+        if(numeros[contador] % 2 == 0){
+            const novoSpan = document.createElement('span')
+            novoSpan.textContent = numeros[contador]
+            container.appendChild (novoSpan)
+        }
+    }
+}
+
+function mostrarImpares(){
+    const numeros = document.getElementById('numeros').value.split(',')
+    const container = document.getElementById('cont-mostrar-impares')
+
+    container.replaceChildren('')
+    const ultimoIndice = numeros.length
+
+    for(let contador = 0; contador < ultimoIndice ; contador ++){
+        if(numeros[contador] % 2 == 1){
+            const novoSpan = document.createElement('span')
+            novoSpan.textContent = numeros[contador]
+            container.appendChild (novoSpan)
+        }
+    }
+}
+
+botaoMostrarImpares.addEventListener('click', mostrarImpares)
+
+botaoMostrarPares.addEventListener('click', mostrarPares)
 
 mostrarTodos.addEventListener('click', mostrarTodosNumeros)
