9cb27e4cb15e24a37eae1339a6f6487a7a87ea7e
NPS <nicolaspetri18@gmail.com>
2023-08-23 14:50:52 -0300

    final com o leo

--- aula4/app.js
+++ aula4/app.js
@@ -1,1 +1,19 @@
 'use strict'
+
+const mostrarTodos = document.getElementById('mostrar-todos')
+
+function mostrarTodosNumeros(){
+    const numeros = document.getElementById('numeros').value.split(',')
+    const container = document.getElementById('container-numeros')
+ 
+    const ultimoIndice = numeros.length
+
+    for(let contador = 0; contador < ultimoIndice ; contador ++){
+        const novoSpan = document.createElement('span')
+        novoSpan.textContent = numeros[contador]
+        container.appendChild(novoSpan)
+    }
+
+}
+
+mostrarTodos.addEventListener('click', mostrarTodosNumeros)
