8acae925c99525a920790217c5bf1ee58455e702
NPS <nicolaspetri18@gmail.com>
2023-08-30 17:23:43 -0300

    faltando JS

---
+++ aula5.1/app.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const pessoas = [
+    {
+        nome: 'Gabriela',
+        mensagem: 'Petri, você viu o Gu?',
+        quantidade: '2',
+        tempo: 'Há 30 minutos',
+        foto: 'gabriela.jfif',
+        contato: 'Nicolas Petri'
+    }
+]
+
+function mostrarCards(pessoas){
+
+    const 
+
+}
+
+pessoas.forEach(mostrarCards)
