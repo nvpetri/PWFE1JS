6b3efc4970094297d97e68b3901801ff0965b5b1
unknown <nicolaspetri18@gmail.com>
2023-09-13 20:41:21 -0300

    consultando endereço

--- aula7/app.js
+++ aula7/app.js
@@ -1,1 +1,33 @@
 'use strict'
+
+const cepInput = document.getElementById('input')
+
+const loadCep = (info) => {
+
+    const rua = document.getElementById('rua').value = info.logradouro
+
+    const bairro = document.getElementById('bairro').value = info.bairro
+
+    const cidade = document.getElementById('cidade').value = info.localidade
+
+    const estado = document.getElementById('estado').value = info.uf
+
+}
+
+const inputCep = async() => {
+    let input = document.getElementById('input').value
+    let cep = String(input).replace('-', '')
+
+    const api = `https://viacep.com.br/ws/${cep}/json/`
+    const resposta = await fetch(api)
+    const infoCep = await resposta.json()
+
+    loadCep(infoCep)
+}
+
+cepInput.addEventListener('blur',inputCep)
+cepInput.addEventListener('keyup', function(enter){
+    if(enter.key === 'Enter'){
+        inputCep()
+    }
+})
